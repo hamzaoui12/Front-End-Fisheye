dfd7c164bea2999e2c44af2dad2ed52eab8561da
hamzaoui12 <hamzaouimerina@gmail.com>
2024-04-19 21:56:00 +0200

    Feat: creation de la page home

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,63 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+export function photographerTemplate(data) {
+    const { id, name, portrait, tagline, price, country, city } = data;
     const picture = `assets/photographers/${portrait}`;
-
+  
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+
+      const article = document.createElement("article");
+      const link = document.createElement("a");
+      link.href = "./photographer.html?id=" + id;
+      link.ariaLabel = name;
+  
+      const img = document.createElement("img");
+      img.setAttribute("src", picture);
+      img.alt = "Photo du profil de " + data.name;
+      img.setAttribute("class", "photographer_picture");
+  
+
+      const divContact = document.createElement("div");
+      const button = document.querySelector(".contact_button");
+      
+      if (button != null) {
+        divContact.appendChild(button);
+      }
+  
+      const h2 = document.createElement("h2");
+      h2.setAttribute("class", "photographer_name");
+      h2.textContent = name;
+  
+      const div = document.createElement("div");
+      div.setAttribute("class", "infos");
+      const locationInfos = document.createElement("p");
+      locationInfos.setAttribute("class", "photographer_city");
+      locationInfos.textContent = city + ", " + country;
+  
+      const taglineInfos = document.createElement("p");
+      taglineInfos.setAttribute("class", "photographer_tagline");
+      taglineInfos.textContent = tagline;
+  
+      const priceInfos = document.createElement("p");
+      priceInfos.textContent = price + " €" + "/" + "jour";
+      priceInfos.setAttribute("class", "photographer_price");
+  
+      article.appendChild(link);
+      link.appendChild(img);
+      div.appendChild(h2);
+      div.appendChild(locationInfos);
+      div.appendChild(taglineInfos);
+      div.appendChild(priceInfos);
+      article.appendChild(divContact);
+      article.appendChild(div);
+      return article;
     }
-    return { name, picture, getUserCardDOM }
-}
+  
+    return {
+      name,
+      picture,
+      city,
+      country,
+      tagline,
+      price,
+      getUserCardDOM,
+    };
+  }
